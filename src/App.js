a0ac4eabec4d0f7906c5a1b3b8addee0edeb1607
Brent Abruzese <brent.abruzese@gmail.com>
2019-08-22 14:59:09 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import API from 'mg-api-node'
 
 function App() {
+  const [vehicle, setVehicle] = useState('');
+  const api = new API('brent@redbanklimo.com', 'Zsw0wers!', 'Redbanklimo')
+  
+  const login = () => {  
+    api.authenticate((err, result) => {
+      if(err) {
+        console.log('Error', err);
+        return;
+      } else { 
+        console.log(result)
+      }
+    })
+  }
+
+const getVehicleInfo = () => {
+  api.call("Get", {
+    typeName: "DeviceStatusInfo",
+    resultsLimit: 10
+}, function (statusInfos) {
+    const coordinates = [];
+    statusInfos.forEach(function (statusInfo) {
+        coordinates.push({
+            x: statusInfo.longitude,
+            y: statusInfo.latitude
+        });
+    });
+    api.call("GetAddresses", {
+        coordinates: coordinates
+    }, function (addressResults) {
+        for (var i = 0; i < statusInfos.length; i++) {
+            setVehicle({
+                device: statusInfos[i].device,
+                isDriving: statusInfos[i].isDriving,
+                address: addressResults[i]
+            });
+        }
+        console.log(vehicle);
+    });
+  
+  });
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={login}>Login</button>
+      <button onClick={getVehicleInfo}>Get Vehicle Info</button>
     </div>
   );
 }
